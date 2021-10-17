766dd375677f0cd6e7b46bec6597e70fe315c3e5
pososix <74709275+pososix@users.noreply.github.com>
2021-10-17 15:43:14 +0300

    Add files via upload

---
+++ sem1/zlobin/task5_var0( hello )/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string name;
+    //string a;
+    //scanf("%s", &a);
+    //printf("%s%s", "hello, ", &a);
+    cin >> name;
+    cout << "Hello, " << name;
+    return 0;
+}

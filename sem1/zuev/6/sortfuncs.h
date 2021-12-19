58f1f0d62089b0486f315f1e7ded206f899fd450
reeegry <6u8654u4@gmail.com>
2021-12-20 01:33:25 +0300

    3, 6

--- sem1/zuev/6/sortfuncs.h
+++ sem1/zuev/6/sortfuncs.h
@@ -1,10 +1,11 @@
+#pragma once
 #ifndef T6_3_SORTFUNCS_H
 #define T6_3_SORTFUNCS_H
 #include <string>
 #include <vector>
 using namespace std;
-vector <string> quicksort(vector <string> v);
-vector <string> bubblesort(vector<string> v);
-vector <string> selectionsort(vector<string> v);
-vector <string> insertsort (vector<string> v);
+vector <string> quicksort(vector <string> &v);
+vector <string> bubblesort(vector<string> &v);
+vector <string> selectionsort(vector<string> &v);
+vector <string> insertsort(vector<string> &v);
 #endif //T6_3_SORTFUNCS_H

20a3ea929d7028e571862adf956f823f31c86012
pososix <74709275+pososix@users.noreply.github.com>
2021-12-23 17:57:35 +0300

    Update main.cpp

--- sem1/zlobin/task3_var1( quad )/main.cpp
+++ sem1/zlobin/task3_var1( quad )/main.cpp
@@ -1,28 +1,51 @@
 #include <iostream>
 #include <cmath>
+#include <clocale>
+
 using namespace std;
 
 int main()
 {
-    float a, b, c, res;
-    printf("Enter number a, b, c: ");
-    scanf("%f%f%f", &a, &b, &c);
-    if((pow(b, 2) - 4*a*c) > 0)
+    long double a,b,c,d,x1,x2;
+    setlocale(LC_CTYPE, "rus");
+    cout << "Введите a\n";
+    cin >> a;
+    cout << "Введите b\n";
+    cin >> b;
+    cout << "Введите c\n";
+    cin >> c;
+    if ((a >= pow(10,153)) or (b >= pow(10,153)) or (c >= pow(10,153)))
     {
-        res = ((-1*b) + sqrt(b*b - 4*a*c)) / (2 * a);
-        printf("%s%.2f", "First solution: ", res);
-
-        res = ((-1*b) - sqrt(b*b - 4*a*c)) / (2 * a);
-        printf("%s%.2f", "\nSolution solution: ", res);
+        cout << "Вы ввели слишком большие значения!" << endl;
+        while((a >= pow(10,153)) or (b >= pow(10,153)) or (c >= pow(10,153)))
+            cin >> a >> b >> c;
+        cout << "-----------" << endl;
     }
-    else if((pow(b, 2) - 4*a*c) == 0)
+    if (a == 0 and b != 0) {
+            cout << ((-c)/b);
+            return 0;
+    }
+    if(a == 0 and b == 0 and c == 0){
+        cout << "Верно для всех чисел" << endl;
+        return 0;
+    }
+    else if (a==0 and b==0){
+            cout << "Нет решений" << endl;
+            return 0;
+    }
+    d = b * b - 4 * a * c;
+    if (d > 0)
     {
-        res = (-1*b + sqrt(b*b - 4*a*c)) / (2 * a);
-        printf("%s%.2f", "Only solution: ", res);
+        x1 = ((-b) + sqrt(d)) / (2 * a);
+        x2 = ((-b) - sqrt(d)) / (2 * a);
+        cout << "x1 = " << x1 << "\n";
+        cout << "x2 = " << x2 << "\n";
     }
-    else
+    if (d == 0)
     {
-        cout << "No solutions..." << endl;
+        x1 = -(b / (2 * a));
+        cout << "x1 = x2 = " << x1 << "\n";
     }
-    return 0;
+    if (d < 0)
+        cout << "D < 0, Действительных корней уравнения не существует";
 }

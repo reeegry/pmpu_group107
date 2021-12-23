8673186bc7dcb6a13a0bafe0552118ddcb8cb017
pososix <74709275+pososix@users.noreply.github.com>
2021-12-23 16:44:43 +0300

    Create sort.h

---
+++ sem1/zlobin/task6_var0( sort )/sort.h
@@ -0,0 +1,17 @@
+#ifndef SORT_H_INCLUDED
+#define SORT_H_INCLUDED
+
+#include <vector>
+
+void print_m(std::vector<std::string> &a, int n);
+
+void sort_b(std::vector<std::string> &a, int n);
+
+void sort_s(std::vector<std::string> &a, int n);
+
+void sort_i(std::vector<std::string> &a, int n);
+
+void sort_q(std::vector<std::string> &a, int l, int r);
+
+
+#endif // SORT_H_INCLUDED

24b68e01924ac150b88567d1c9ed42ed05d6dc5e
pososix <74709275+pososix@users.noreply.github.com>
2021-12-25 21:51:47 +0300

    Update main.cpp

--- sem1/zlobin/task2_var0( bigcalc )/main.cpp
+++ sem1/zlobin/task2_var0( bigcalc )/main.cpp
@@ -1,44 +1,99 @@
 #include <iostream>
 #include <cstdlib>
 #include <algorithm>
+#include <vector>
 
 using namespace std;
 
-string binary(int n);
+void binary(unsigned long long n, int len);
+int length_binary(unsigned long long n);
 
 int main()
 {
-    int a, b;
+    unsigned long long a, b;
     printf("Enter the number A, B: ");
-    scanf("%d%d", &a, &b);
-    cout << "Binary A = " << binary(a) << endl;
-    cout << "Binary B = " << binary(b) << endl;
-    cout << binary(a) << " + " << binary(b) << " = " << binary(a + b) << endl;
-    if(a > b)
+    cin >> a >> b;
+    cout << "Binary A = " << " ";
+    binary(a, length_binary(a));
+    cout << endl;
+    cout << "Binary B = " << " ";
+    binary(b, length_binary(b));
+    cout << endl;
+    binary(a, length_binary(a));
+    cout << " + ";
+    binary(b, length_binary(b));
+    cout << " = ";
+    binary(a+b, length_binary(a+b));
+    cout << endl;
+
+
+    if (a > b)
     {
-        cout << binary(a) << " - " << binary(b) << " = " << binary(a - b) << endl;
+        binary(a, length_binary(a));
+        cout <<  " - ";
+        binary(b, length_binary(b));
+        cout << " = ";
+        binary(a-b, length_binary(a-b));
     }
-    else if(a == b)
+    else if(a==b)
     {
-        cout << binary(a) << " - " << binary(b) << " = " << 0 << endl;
+        cout << "0";
     }
     else
     {
-        cout << binary(a) << " - " << binary(b) << " = " << "-" << binary(b - a) << endl;
+        binary(b, length_binary(b));
+        cout << " - ";
+        binary(a, length_binary(a));
+        cout << " = - ";
+        binary(b-a, length_binary(b-a));
     }
     return 0;
 }
 
-string binary(int n)
+void binary(unsigned long long n, int len)
 {
-    string rez;
-    while(n >= 1)
+    vector<bool> result;
+    unsigned long long nCopy = n, count = 0;
+    while (nCopy != 0)
     {
-        int mod = n % 2;
-        string modstr = to_string(mod);
-        rez += modstr;
-        n = n / 2;
+        count++;
+        nCopy /= 2;
     }
-    reverse(begin(rez), end(rez));
-    return rez;
+    result.resize(count);
+    if (n == 0)
+    {
+        cout << 0;
+    }
+    else
+    {
+        int i = 0;
+        while (n != 0)
+        {
+            i = i + 1;
+            result.push_back(n % 2);
+            n /= 2;
+        }
+        reverse(result.begin(), result.end());
+        vector<bool> new_result;
+        new_result.resize(len);
+        for (int i = len - 1; i >= 0; --i)
+        {
+            new_result.push_back(result[i]);
+        }
+        reverse(new_result.begin(), new_result.end());
+        for(int i = 0; i < len; i++)
+            cout << new_result[i];
+    }
+}
+int length_binary(unsigned long long n)
+{
+    unsigned long long copy_n_2 = n;
+    int length = 0;
+    while(copy_n_2 >= 1)
+    {
+        int mod = copy_n_2 % 2;
+        copy_n_2 = copy_n_2 / 2;
+        length++;
+    }
+    return length;
 }
